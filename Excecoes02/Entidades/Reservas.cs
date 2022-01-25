2bb222486c673108a84154579f8ba6041b4e0fcf
Alex Marcelo <alexm9002@gmail.com>
2022-01-25 15:32:04 -0300

    Melhoras no Programa exercício de Excecoes

---
+++ Excecoes02/Entidades/Reservas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Excecoes.Entidades {
+    internal class Reservas {
+        public int NumeroQuarto { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+
+        public Reservas(int numeroQuarto, DateTime entrada, DateTime saida) {
+            this.NumeroQuarto = numeroQuarto;
+            this.Entrada = entrada;
+            this.Saida = saida;
+        }
+        public int Duracao() {
+            // TimeSpan é usado para calcular duração de um período
+            TimeSpan duracao = Saida.Subtract(Entrada);
+
+            // Para transformar em dias usa-se a função "Total.Days"
+            // transformando os dados em inteiro, pois o retorno do
+            // "TotalDays" é feito em formato "Double".
+            return (int)duracao.TotalDays;
+        }
+        public String AtualizarData(DateTime entrada, DateTime saida) {
+            // Como a função vai retornar uma mensagem, ela passa a ser
+            // do tipo "String" ao invés de "void"
+            DateTime hoje = DateTime.Now;
+            if (entrada < hoje || saida < hoje) {
+                return "Erro na reserva! A data lançada para reserva precisa ser futura";
+            //Não usa o "else", o return desconsidera o restante
+            }
+            if (saida <= entrada) {
+                return "Erro na reserva! Data de Check-out não pode ser anterior a Data de Check-in ";
+            }
+            this.Entrada = entrada;
+            this.Saida = saida;
+            return null; // retorna indicando que não houve nenhum erro no objeto 
+        }
+
+        public override string ToString() {
+            return "Quarto: "
+            + NumeroQuarto
+            + ", Check-in: "
+            + Entrada.ToString("dd/MM/yyyy")
+            + ", Check-out: "
+            + Saida.ToString("dd/MM/yyyy")
+            + ", "
+            + Duracao() //Colocar a função e não a variável.
+            + " Noites.";
+        }
+    }
+}
---
+++ Excecoes02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Excecoes.Entidades;
+
+
+namespace Excecoes {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Número do Quarto: ");
+            int numeroQuarto = int.Parse(Console.ReadLine());
+            Console.Write("Data do Check-in (dd/MM/yyyy): ");
+            DateTime entrada = DateTime.Parse(Console.ReadLine());
+            Console.Write("Data do Check-out (dd/MM/yyyy):");
+            DateTime saida = DateTime.Parse(Console.ReadLine());
+
+            if (saida <= entrada) {
+                Console.WriteLine("Erro na reserva! Data de Check-out não pode ser anterior a Data de Check-in ");
+            }
+            else {
+                Reservas reservas = new Reservas(numeroQuarto, entrada, saida);
+                Console.WriteLine("Reserva --> " + reservas);
+                Console.WriteLine();
+                Console.WriteLine("Entre com os dados para atualizar a reserva!");
+                Console.Write("Data do Check-in (dd/MM/yyyy): ");
+                entrada = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data do Check-out (dd/MM/yyyy):");
+                saida = DateTime.Parse(Console.ReadLine());
+
+                string erro = reservas.AtualizarData(entrada, saida);
+                if (erro != null) {
+                    Console.WriteLine("Erro na reserva" + erro);
+                }
+                else {
+                    Console.WriteLine("Reservas: " + reservas);
+                }
+            }
+        }
+    }
+}
